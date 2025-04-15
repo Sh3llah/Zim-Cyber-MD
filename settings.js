690efc687df9054a2902be3da80b333f666562cc
Sh3llah <sh3llahcyber7@gmail.com>
2025-04-15 09:05:55 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'sh3llah',
+  author: 'Bot',
+  botName: "Zim Cyber-MD",
+  botOwner: 'Mr Shellah', // Your name
+  ownerNumber: '263771341158', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
